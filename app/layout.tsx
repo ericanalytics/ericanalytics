330dee83966b6d8d66261ce042cf63be4385586b
ericanalytics <ericanalytics0287@gmail.com>
2024-10-08 17:37:57 +0530

    issue fixing

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`eric-analytics`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>

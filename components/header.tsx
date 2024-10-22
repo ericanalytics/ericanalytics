fc40acace2d50cda7de427016739b9458a68803d
ericanalytics <ericanalytics0287@gmail.com>
2024-10-22 13:48:40 +0530

    chk

--- components/header.tsx
+++ components/header.tsx
@@ -1,3 +1,4 @@
+// components/Header.tsx
 import React from 'react';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
@@ -6,10 +7,7 @@ const Header: React.FC = () => {
     const router = useRouter();
 
     const scrollToSection = (sectionId: string) => {
-        // Change the URL without reloading the page
         router.push(`/${sectionId}`);
-        
-        // Smoothly scroll to the section
         const section = document.getElementById(sectionId);
         if (section) {
             section.scrollIntoView({ behavior: 'smooth' });

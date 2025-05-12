019fdc1367590c922850886e24eb073aaf6111df
ericanalytics <ericanalytics0287@gmail.com>
2025-05-12 17:54:40 +0530

    facicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-
+ <head>
+ <link rel="icon" href="/favicon.png" type="image/x-icon" sizes="16x16"/>
+ </head>
       <body style={{ background: '#000', fontFamily: 'Inter, sans-serif'}}
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >

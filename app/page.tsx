541991ac362ec0487e048d8ff60e5574b446eccb
ericanalytics <ericanalytics0287@gmail.com>
2025-05-23 16:51:07 +0530

    google-site-update

--- app/page.tsx
+++ app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
       <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet" />
       <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet" />
       <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
-        <meta name="google-site-verification" content="dkHfE2aXKPhzbglrht7IvsCHbOos63FWj3kyTT4JNvM" />
+
 
     <div>
     <Header />
